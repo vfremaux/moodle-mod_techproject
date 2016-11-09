52c68bc5723f96eefdad9512ee06897a9b9574e9
valery <valery.fremaux@gmail.com>
2016-11-09 21:37:01 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -17,20 +17,19 @@
 /**
  *  Code fragment to define the version of project
  *
- * @package mod_techproject
- * @subpackage framework
- * @author Valery Fremaux (France) (admin@www.ethnoinformatique.fr)
- * @date 2008/03/03
- * @version phase1
+ * @package     mod_techproject
+ * @category    mod
+ * @author      Valery Fremaux (France) (admin@www.ethnoinformatique.fr)
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2015101300;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2015111100;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2015111100;  // Requires this Moodle version
 $plugin->component = 'mod_techproject';   // Full name of the plugin (used for diagnostics)
-$plugin->cron     = 0;           // Period for cron to check this module (secs)
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.0.0 (Build 2015101300)';
+$plugin->release = '3.0.0 (Build 2015111100)';
 
+// Non Moodle attributes.
+$plugin->codeincrement = '3.0.0000';
