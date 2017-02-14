9721b79d4a3cb35f0024235c3c8be7a8709cb30d
valery <valery.fremaux@gmail.com>
2017-02-14 12:56:20 +0100

    Firt version load

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2015101300;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2016052300;  // Requires this Moodle version
-$plugin->component = 'mod_techproject';   // Full name of the plugin (used for diagnostics)
+$plugin->version  = 2015101300;  // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2016052300;  // Requires this Moodle version.
+$plugin->component = 'mod_techproject';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.1.0 (Build 2015101300)';
+$plugin->release = '3.2.0 (Build 2015101300)';
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.1.0001';
+$plugin->codeincrement = '3.2.0001';
