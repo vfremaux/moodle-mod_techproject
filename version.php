2412f71112927c33ff65fbe3ff933cfe7d3ed1c4
valery <valery.fremaux@gmail.com>
2018-10-08 16:40:51 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2017040200;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2016112900;  // Requires this Moodle version.
+$plugin->requires = 2018050800;  // Requires this Moodle version.
 $plugin->component = 'mod_techproject';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.2.0 (Build 2017040200)';
+$plugin->release = '3.5.0 (Build 2017040200)';
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.2.0003';
+$plugin->codeincrement = '3.5.0003';
