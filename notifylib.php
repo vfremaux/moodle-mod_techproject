f7e0d1edbd621a72ef81a9ab57ba622831f5b6fe
valery <valery.fremaux@gmail.com>
2020-07-11 12:02:14 +0200

    travis update

--- notifylib.php
+++ notifylib.php
@@ -135,6 +135,43 @@ function techproject_notify_new_requirement(&$project, $cmid, &$requirement, $cu
 }
 
 /**
+ * Notifies all project managers of a new requirement being entered
+ * @param objectref &$project
+ * @param int $cmid
+ * @param objectref &$requirement
+ * @param int $currentgroupid
+ */
+function techproject_notify_new_deliverable(&$project, $cmid, &$deliverable, $currentgroupid) {
+    global $USER, $COURSE, $CFG, $DB;
+
+    techproject_complete_user($USER);
+
+    $class = get_string('deliverable', 'techproject');
+    $projectheading = $DB->get_record('techproject_heading', array('projectid' => $project->id, 'groupid' => $currentgroupid));
+    $message = techproject_compile_mail_template('newentrynotify', array(
+        'PROJECT' => $projectheading->title,
+        'CLASS' => $class,
+        'USER' => fullname($USER),
+        'ENTRYNODE' => implode(".", techproject_tree_get_upper_branch('techproject_deliverable', $deliverable->id, true, true)),
+        'ENTRYABSTRACT' => stripslashes($deliverable->abstract),
+        'ENTRYDESCRIPTION' => $deliverable->description,
+        'QUALIFIERS' => implode('<br/>', $qualifiers),
+        'ENTRYLINK' => $CFG->wwwroot."/mod/techproject/view.php?id={$cmid}&view=deliverables&group={$currentgroupid}"
+    ), 'techproject');
+    $context = context_module::instance($cmid);
+    $fields = 'u.id,'.get_all_user_name_fields(true, 'u').',u.username, u.email, picture, mailformat';
+    $managers = get_users_by_capability($context, 'mod/techproject:manage', $fields);
+
+    if (!empty($managers)) {
+        foreach ($managers as $manager) {
+            techproject_complete_user($manager);
+            $subject = $COURSE->shortname .' - '.get_string('notifynewrequ', 'techproject');
+            email_to_user($manager, $USER, $subject, html_to_text($message), $message);
+        }
+    }
+}
+
+/**
  * Notifies all project managers of a new task being entered
  * @param objectref &$project
  * @param int $cmid
